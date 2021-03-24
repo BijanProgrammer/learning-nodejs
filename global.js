00e77f31595ddfa454346c992a09564a05855c10
BijanProgrammer <bijaneisapour@gmail.com>
2021-03-24 13:48:30 +0430

    [EXPERIMENT] fs

---
+++ global.js
@@ -0,0 +1,5 @@
+console.log('Hello, friend!');
+
+setTimeout(() => {
+    console.log('3 seconds later ...');
+}, 3000);
---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ['Bijan', 'Samin', 'Reza'];
+const ages = [20, 21, 19];
+
+module.exports = {
+    people,
+    ages,
+};
